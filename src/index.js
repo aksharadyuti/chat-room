de31e3c9d8aff4d37e1d9f43656302576ac3fdff
aksharadyuti <47235698+aksharadyuti@users.noreply.github.com>
2020-08-01 18:52:08 +0530

    use express

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ const express = require('express')
 const app = express()
 
 const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname,'../public')
 
-app.listen(port)
+app.use(express.static(publicDirectoryPath))
+
+app.listen(port,()=>{
+    console.log(`Server up on port ${port}`)
+
+})
