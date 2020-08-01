0f8edaf7757884332520266d0d2d6a6a852dd5c8
aksharadyuti <47235698+aksharadyuti@users.noreply.github.com>
2020-08-01 19:41:05 +0530

    socket.io events playground

--- src/index.js
+++ src/index.js
@@ -12,8 +12,17 @@ const publicDirectoryPath = path.join(__dirname,'../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection',()=>{
+let count = 0
+io.on('connection',(socket)=>{
 console.log('New web socket connection')
+//emits to specific connection
+socket.emit('countUpdated',count)
+socket.on('increment', ()=>{
+    count++
+    //emits to all
+    io.emit('countUpdated',count)
+})
+
 })
 
 server.listen(port,()=>{
