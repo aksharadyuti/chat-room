be8ada91a6d394faf7ad35147ef5f6a68b1ecbad
aksharadyuti <47235698+aksharadyuti@users.noreply.github.com>
2020-08-10 22:15:33 +0530

    add acknowledgements

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,7 +6,12 @@ socket.on('message',(msg)=>{
 document.querySelector('#message-form').addEventListener('submit',(e)=>{
     e.preventDefault()
     const message = e.target.elements.message.value
-    socket.emit('sendMessage',message)
+    socket.emit('sendMessage',message,(error)=>{
+        if(error){
+            return console.log(error)
+        }
+        console.log('Message delivered');
+    })
 
 })
 
@@ -19,8 +24,10 @@ document.querySelector('#send-location').addEventListener('click',()=>{
             socket.emit('sendLocation', {
                 latitude: position.coords.latitude,
                 longitude: position.coords.longitude
+            },()=>{
+                console.log('Location shared');
             })
-            
+
         })
     }
 })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -13,14 +14,28 @@ const publicDirectoryPath = path.join(__dirname,'../public')
 app.use(express.static(publicDirectoryPath))
 
 io.on('connection',(socket)=>{
+
     console.log('New web socket connection')
+
     socket.emit('message','Welcome')
+
     socket.broadcast.emit('message','New user joined');
-    socket.on('sendMessage',(message)=>{
+
+    socket.on('sendMessage',(message,callback)=>{
+        const filter = new Filter()
+        if(filter.isProfane(message)){
+            return callback('Profanity is not allowed')
+        }
+
         io.emit('message',message)
+        callback('Delivered')
+
+        
     })
-    socket.on('sendLocation',(coords)=>{
+
+    socket.on('sendLocation',(coords, callback)=>{
         io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
     })
     socket.on('disconnect',()=>{
         io.emit('message','User disconnected')
