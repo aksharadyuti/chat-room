9823eec1503fd15cc01e50b4680fb4baffd536a8
aksharadyuti <47235698+aksharadyuti@users.noreply.github.com>
2020-08-11 06:04:18 +0530

    socket.io rooms

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,6 +10,9 @@ const messageTemplate = document.querySelector('#message-template').innerHTML
 
 const locationTemplate = document.querySelector('#location-template').innerHTML
 
+//options
+const{username, room} = Qs.parse(location.search, {ignoreQueryPrefix: true})
+
 socket.on('message',(message)=>{
     console.log(message)
     const html = Mustache.render(messageTemplate,{
@@ -62,4 +65,6 @@ document.querySelector('#send-location').addEventListener('click',()=>{
         })
     }
 })
+
+socket.emit('join', { username, room})
 
--- src/index.js
+++ src/index.js
@@ -17,18 +17,21 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection',(socket)=>{
 
     console.log('New web socket connection')
+    socket.on('join',({username, room})=>{
+        socket.join(room)
+        socket.to(room).emit('message',generateMessage('Welcome'))
+        socket.broadcast.to(room).emit('message',generateMessage(`${username} has joined`));
+    
 
-    socket.emit('message',generateMessage('Welcome'))
-
-    socket.broadcast.emit('message',generateMessage('New user joined'));
+    })
 
     socket.on('sendMessage',(message,callback)=>{
         const filter = new Filter()
         if(filter.isProfane(message)){
             return callback('Profanity is not allowed')
         }
-
-        io.emit('message',generateMessage(message))
+        //hardcode room name
+        io.to('1').emit('message',generateMessage(message))
         callback('Delivered')
         
 
